ea6f3e31d4796c9d5a406a02a709ea07a5fbed0c
kidchang93 <lck930721@gmail.com>
2024-06-01 17:15:09 +0900

    새 채팅방만드는 메서드

--- src/main/java/com/ckhub/websocket/chatroom/ChatRoomRepository.java
+++ src/main/java/com/ckhub/websocket/chatroom/ChatRoomRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
-    Optional<String> findBySenderIdAndRecipientId(String senderId, String recipientId);
+
+    // ChatRoom 객체여야함
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
 }
--- src/main/java/com/ckhub/websocket/chatroom/ChatRoomService.java
+++ src/main/java/com/ckhub/websocket/chatroom/ChatRoomService.java
@@ -15,9 +15,45 @@ public class ChatRoomService {
 
     // 채팅방을 가져오는 메서드
     // 플래그 - 존재하지 않는 경우 새방 만들기를 호출하는 변수
-    public Optional<String> getChatRoomId (String senderId, String recipientId, boolean createNewRoomIfNotExists){
-        return chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId);
+    public Optional<String> getChatRoomId (String senderId,
+                                           String recipientId,
+                                           boolean createNewRoomIfNotExists){
+        // 없으면 새 방을 만들 것이므로 람다 표현식을 써서 만들어 준다.
+        // 여기서 발신자 ID 수신자 ID를 찾으려고 하지만 이는 채팅방의 옵션을 반환하므로
+        // 매핑해야한다.
+        return chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId)
+                .map(ChatRoom::getChatId)
+                .or(()-> {
+                    if (createNewRoomIfNotExists){
+                        var chatId = createChatId(senderId,recipientId);
+                        return Optional.of(chatId);
+                    }
+                    return Optional.empty();
+                });
 
 
+    }
+
+    // 위에서 쓸 메서드를 만든다.
+    private String createChatId(String senderId, String recipientId) {
+        // 채팅 아이디에 대한 형식을 정해 변수에 담는다.
+        var chatId = String.format("%s_%s", senderId,recipientId); // ck_ckhub
+
+        // 각 채팅방의 발신자와 수신자ID를 전환하여 보낸 사람과 받는 사람을 위한 두개의 채팅방을 만듭니다.
+        ChatRoom senderRecipient = ChatRoom.builder()
+                .chatId(chatId)
+                .senderId(senderId)
+                .recipientId(recipientId)
+                .build();
+
+        ChatRoom recipientSender = ChatRoom.builder()
+                .chatId(chatId)
+                .senderId(senderId).
+                recipientId(recipientId)
+                .build();
+
+        chatRoomRepository.save(senderRecipient);
+        chatRoomRepository.save(recipientSender);
+        return chatId;
     }
 }
